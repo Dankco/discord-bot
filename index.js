cf789f14287133533f2a7b3fbff3e6438f9dab96
Daniel Kao <dack8@outlook.com>
2023-04-25 08:39:49 -0700

    add new content to bot

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express()
 
+require('dotenv').config()
 app.listen(3000, () => {
   console.log("Project is running!")
 })
@@ -9,7 +10,7 @@ app.get("/",(req,res) =>{
   res.send("Hello world!")
 })
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits} = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -21,18 +22,24 @@ const client = new Client({
   ]
 })
 
-
-const msgs = ["https://www.youtube.com/watch?v=Rpc-ZIRcUrA", "Yan of the Yulun Family", "Where's my Yanyulun?", "Yanyuliners for dinner!", "Yulun some Yulose some", "My name is Yanyulun!", "Pogyulun!", "This is true!", "Dao mei",  'in the psych ward, 13cs', "the mid lane doesn’t exist!"];
+man_bot = './public/4_man_bot.png'
+subscribe = './public/subscribe.png'
+const msgs = ["https://www.youtube.com/watch?v=Rpc-ZIRcUrA", "Yan of the Yulun Family", "Where's my Yanyulun?", 
+"Yanyuliners for dinner!", "Yulun some Yulose some", "My name is Yanyulun!", "Pogyulun!", 
+"This is true!", "Dao mei",  'in the psych ward, 13cs', "the mid lane doesn’t exist!", 
+{ files: [man_bot] }, {content: "Subscribe to yanyulun's new Youtube channel!",files: [subscribe]}];
+const feed_msgs = ["Dao Mei", "Wo De Yanyulun Mei Le", "Ai ya!"]
 
 client.on('messageCreate', message => {
   if(message.author.bot)
   {
     return
   }
-  const content = message.content;
-  const msg_number = Math.floor(Math.random() * msgs.length);
+  const content = message.content.toLowerCase();
+  const msg_number = msgs.length-1;
+  const feed_msg_number = Math.floor(Math.random() * feed_msgs.length)
   const where_yanyulun = 'where is my yanyulun'
-  if(content.toLowerCase().match(where_yanyulun))
+  if(content.match(where_yanyulun))
   {
     // First use guild.members.fetch to make sure all members are cached
     message.guild.members.fetch({ withPresences: true }).then(fetchedMembers => {
@@ -46,7 +53,11 @@ client.on('messageCreate', message => {
       else
         message.channel.send("Yanyulun mei le!");
     });
-  } else if(content.toLowerCase().match('yanyulun'))
+  } else if(content.match("feeding") || content.match("inting"))
+  {
+    message.channel.send(feed_msgs[feed_msg_number]);
+  }
+  else if(content.match('yanyulun'))
   {
     message.channel.send(msgs[msg_number]);
   }
